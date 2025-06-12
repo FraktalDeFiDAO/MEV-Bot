2b73f2d40f9cee3205a212509c7459d810565aaf
FraktalDeFiDAO <131066313+FraktalDeFiDAO@users.noreply.github.com>
2025-06-12 15:18:33 -0500

    Add Foundry setup and Go bot scaffold

---
+++ cmd/bot/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "context"
+    "log"
+    "github.com/ethereum/go-ethereum/ethclient"
+)
+
+// Entry point for the MEV bot. Connects to an Arbitrum node and listens for events.
+func main() {
+    // replace with your Arbitrum RPC endpoint
+    rpcURL := "https://arb1.arbitrum.io/rpc"
+
+    client, err := ethclient.DialContext(context.Background(), rpcURL)
+    if err != nil {
+        log.Fatalf("failed to connect to arbitrum rpc: %v", err)
+    }
+    defer client.Close()
+
+    log.Println("connected to arbitrum", rpcURL)
+
+    // TODO: subscribe to dex events, transactions and sequencer feed
+}
+
