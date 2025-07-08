3f4f0b81a02d4da1274a6ae0744926d0eb78341d
Derek Williams <betteryourweb@gmail.com>
2025-07-07 19:40:26 -0500

    Add event watcher

---
+++ cmd/bot/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/FraktalDeFiDAO/MEV-Bot/pkg/ethutil"
+)
+
+// Entry point for the MEV bot. Connects to an Arbitrum node and listens for events.
+
+func run(ctx context.Context, rpcURL string) error {
+	client, err := ethutil.ConnectClient(ctx, rpcURL)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	if _, err := client.ChainID(ctx); err != nil {
+		return err
+	}
+
+	log.Println("connected to arbitrum", rpcURL)
+	return nil
+}
+
+func main() {
+	rpcURL := "https://arb1.arbitrum.io/rpc"
+
+	if err := run(context.Background(), rpcURL); err != nil {
+		log.Fatalf("failed to connect to arbitrum rpc: %v", err)
+	}
+
+	// TODO: subscribe to dex events, transactions and sequencer feed
+}
---
+++ pkg/watcher/watcher.go
@@ -0,0 +1,86 @@
+package watcher
+
+import (
+	"context"
+	"log"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+// HeaderSubscriber wraps SubscribeNewHead so it can be mocked in tests.
+type HeaderSubscriber interface {
+	SubscribeNewHead(ctx context.Context, ch chan<- *types.Header) (ethereum.Subscription, error)
+}
+
+// BlockWatcher listens for new block headers using an Ethereum client.
+type BlockWatcher struct {
+        sub HeaderSubscriber
+}
+
+// NewBlockWatcher creates a watcher using the given subscriber.
+func NewBlockWatcher(sub HeaderSubscriber) *BlockWatcher {
+	return &BlockWatcher{sub: sub}
+}
+
+// Run subscribes to new block headers and logs them until the context is done.
+func (bw *BlockWatcher) Run(ctx context.Context) error {
+	headers := make(chan *types.Header)
+	sub, err := bw.sub.SubscribeNewHead(ctx, headers)
+	if err != nil {
+		return err
+	}
+	defer sub.Unsubscribe()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case err := <-sub.Err():
+			if err != nil {
+				return err
+			}
+		case h := <-headers:
+			log.Printf("new block: %d", h.Number.Uint64())
+		}
+	}
+}
+
+// LogSubscriber wraps SubscribeFilterLogs so it can be mocked in tests.
+type LogSubscriber interface {
+        SubscribeFilterLogs(ctx context.Context, q ethereum.FilterQuery, ch chan<- types.Log) (ethereum.Subscription, error)
+}
+
+// EventWatcher listens for contract events based on a filter query.
+type EventWatcher struct {
+        sub   LogSubscriber
+        query ethereum.FilterQuery
+}
+
+// NewEventWatcher creates an event watcher for the given query.
+func NewEventWatcher(sub LogSubscriber, q ethereum.FilterQuery) *EventWatcher {
+        return &EventWatcher{sub: sub, query: q}
+}
+
+// Run subscribes to logs and prints their transaction hash until the context ends.
+func (ew *EventWatcher) Run(ctx context.Context) error {
+        logsCh := make(chan types.Log)
+        sub, err := ew.sub.SubscribeFilterLogs(ctx, ew.query, logsCh)
+        if err != nil {
+                return err
+        }
+        defer sub.Unsubscribe()
+
+        for {
+                select {
+                case <-ctx.Done():
+                        return ctx.Err()
+                case err := <-sub.Err():
+                        if err != nil {
+                                return err
+                        }
+                case l := <-logsCh:
+                        log.Printf("log tx: %s", l.TxHash.Hex())
+                }
+        }
+}
